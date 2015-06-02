58df1f7f6675fdc7e29c69191cda8b5de995140d
Florent PERGOUD <florentpergoud@gmail.com>
2015-06-02 18:50:58 +0200

    Fix rabin-miller

--- examples/rabin-miller.cpp
+++ examples/rabin-miller.cpp
@@ -5,22 +5,23 @@
 int main(int argc, char const *argv[])
 {
 	int k;
+	double probability;
 	std::string nb;
-	std::cout << "Entrer un nombre a tester" << std::endl;
+	std::cout << "Enter a number to test" << std::endl;
 	std::cin >> nb;
 
-	std::cout << "Entrer le nombre de test à faire" << std::endl;
+	std::cout << "How many test would you want to do? " << std::endl;
 	std::cin >> k;
 
 	if (crypto_primality_test(mpz2_class(nb), k))
 	{
-		std::cout << "Le nombre est très probablement premier" << std::endl;
+		std::cout << "The number is probably prime" << std::endl;
 		probability= pow(4,-k);
-	std::cout << "La probabilité d'avoir déclaré " << n << " probablement premier alors qu'il ne l'est pas est de " << probability << std::endl;
+	std::cout << "Mistake probability is of the order of " << probability << std::endl;
 	}
 	else
 	{
-		std::cout << "Le nombre n'est pas premier" << std::endl;
+		std::cout << "The number is composite" << std::endl;
 	}
 	return 0;
 }
